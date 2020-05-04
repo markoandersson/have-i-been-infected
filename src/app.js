ba861675f1de60778c488d40dddc21dca0534583
Marko Andersson <marko.andersson@solteq.com>
2020-05-04 15:37:14 +0300

    Use AlpineJS to control element visibilities

--- src/app.js
+++ src/app.js
@@ -1,90 +1,53 @@
+import 'alpinejs'
 import { createStateMachine } from "./state";
 
-const showAnalyzingIndicator = (show) => {
-
-    const analyzingIndicator = document.getElementById('analyzing-indicator')
-    const childDivs = document.getElementsByClassName('dot-loader')
-
-    analyzingIndicator.style.display = !show ? "none" : "flex"
-    Array.from(childDivs).forEach(element => {
-        element.hidden = !show
-    });
-}
-const show = (element) => {
-    element.classList.remove('hidden')
-}
-
-const hide = (element) => {
-    element.classList.add('hidden')
-}
-
-const afterPageLoad = () => {
-
-    const fingerprintReader = document.getElementById('fingerprint-scanner')
-    const textLabel = document.getElementById('label');
-    const resetButton = document.getElementById('reset')
-    const scanningAnimation = document.getElementById('scanning-animation')
-    const backgroundImage = document.getElementById('background-image')
-
-    const machine = createStateMachine((state) => {
-
-
-        if (state === 'idle') {
-            textLabel.innerText = 'Place your finger to the reader and wait for the measurement to complete'
-            textLabel.classList.remove('text-green-600', 'text-red-600', 'text-3xl')
-            textLabel.classList.add('text-gray-100', 'text-xl')
-            hide(resetButton)
-            hide(scanningAnimation)
-            showAnalyzingIndicator(false)
-            show(fingerprintReader)
-            backgroundImage.classList.remove('deblur')
-            fingerprintReader.classList.add('pulse')
-
-        } else if (state === 'measuring') {
-            textLabel.innerText = 'Measuring...'
-            show(scanningAnimation)
-            fingerprintReader.classList.remove('pulse')
-        } else if (state === 'analyzing') {
-            textLabel.innerText = 'Analyzing the results...'
-            hide(scanningAnimation)
-            hide(fingerprintReader)
-            showAnalyzingIndicator(true)
-            backgroundImage.classList.add('deblur')
-        } else if (state === 'healthy') {
-            textLabel.innerText = 'You are probably healthy'
-            show(resetButton)
-            textLabel.classList.remove('text-gray-100', 'text-xl')
-            textLabel.classList.add('text-green-600', 'text-3xl')
-            showAnalyzingIndicator(false)
-        } else if (state === 'infected') {
-            textLabel.innerText = 'You migth have been infected'
-            textLabel.classList.remove('text-gray-100', 'text-xl')
-            textLabel.classList.add('text-red-600', 'text-3xl')
-            show(resetButton)
-            showAnalyzingIndicator(false)
-        } else if (state === 'not_mobile') {
-            hide(fingerprintReader)
-            textLabel.innerText = 'Unfortunatelly this application works only with mobile devices.'
+window.HaveIBeenInfected = function() {
+  
+    return {
+        state: '',
+        init() {
+            this.machine = createStateMachine((state) => {
+                this.state = state
+            })
+        },
+        pressScanner() {
+            this.machine.send('ON_FINGER_DOWN')
+        },
+        releaseScanner() {
+            this.machine.send('ON_FINGER_UP')
+        },
+        measureAgain() {
+            this.machine.send('RESET')
+        },
+        showFingerprintScanner() {
+            return this.isIdle() || this.isMeasuring()
+        },
+        showResetButton() {
+            return this.isHealthy() || this.isInfected()
+        },
+        deblurBackground() {
+            return this.isAnalyzing() || this.isHealthy() || this.isInfected()
+        },
+        isIdle() {
+            return this.state === 'idle'
+        },
+        isMeasuring() {
+            return this.state === 'measuring'
+        },
+        isAnalyzing() {
+            return this.state === 'analyzing'
+        },
+        isHealthy() {
+            return this.state === 'healthy'
+        },
+        isInfected() {
+            return this.state === 'infected'
+        },
+        isNotMobile() {
+            return this.state === 'not_mobile'
         }
-    })
-
-    fingerprintReader.addEventListener('touchstart', () => {
-        machine.send('ON_FINGER_DOWN')
-    }, { passive: true })
-
-    fingerprintReader.addEventListener('touchend', () => {
-        machine.send('ON_FINGER_UP')
-
-    }, { passive: true })
-
-    resetButton.addEventListener('click', () => {
-        machine.send('RESET')
-    }, { passive: true })
+    }
 }
-
-window.addEventListener('load', () => {
-    afterPageLoad()
-})
 
 window.addEventListener('contextmenu', (event) => {
     event.preventDefault()
